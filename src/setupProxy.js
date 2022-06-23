bd0afa578f2b3510a1724ac1e7e0fc2ba686e60e
sekhyuni <sekhyuni@gmail.com>
2022-06-23 22:32:26 +0900

    proxy관련 설정 (setupProxy.js)

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = {
+    target: 'http://localhost:8080'
+}
+
+module.exports = (app) => {
+    app.use(
+        '/api',
+        createProxyMiddleware(proxy)
+    );
+};
